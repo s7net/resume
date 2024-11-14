d393cea6f78c1d9a4c7ce73ecda7322761b9ac5c
Bridger Tower <bridgertower@gmail.com>
2024-11-14 14:55:33 -0700

    wip

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,5 +58,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("tailwindcss-motion")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
